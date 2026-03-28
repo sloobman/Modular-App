9581e1af59a3c3c0a3187b2feca66627235e97ec
sloobman <user@email.com>
2026-03-28 12:05:58 +0300

    Implement IModule interface with service registration and initialization methods

---
+++ Modules/IModule.cs
@@ -0,0 +1,8 @@
+public interface IModule
+{
+    string Name { get; }
+    IEnumerable<string> Dependencies { get; }
+
+    void RegisterServices(IServiceCollection services);
+    void Initialize(IServiceProvider provider);
+}
