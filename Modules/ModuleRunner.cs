d7928fc8da93c23e03681be8bf4990d67743b5aa
sloobman <user@email.com>
2026-03-28 12:07:15 +0300

    Add ModuleRunner class to execute and initialize modules with service registration

---
+++ Modules/ModuleRunner.cs
@@ -0,0 +1,20 @@
+public class ModuleRunner
+{
+    public void Run(List<IModule> modules)
+    {
+        var services = new ServiceCollection();
+
+        foreach (var module in modules)
+        {
+            module.RegisterServices(services);
+        }
+
+        var provider = services.BuildServiceProvider();
+
+        foreach (var module in modules)
+        {
+            Console.WriteLine($"Запуск: {module.Name}");
+            module.Initialize(provider);
+        }
+    }
+}
