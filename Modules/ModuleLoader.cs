97ec879dc39d94ada46d169a6fdd45448cf49269
sloobman <user@email.com>
2026-03-28 12:06:18 +0300

    Add ModuleLoader class to dynamically load IModule implementations

---
+++ Modules/ModuleLoader.cs
@@ -0,0 +1,11 @@
+public class ModuleLoader
+{
+    public List<IModule> LoadModules()
+    {
+        return Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(t => typeof(IModule).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+            .Select(t => (IModule)Activator.CreateInstance(t)!)
+            .ToList();
+    }
+}
