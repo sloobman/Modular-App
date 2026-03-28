acde9ebcf0b4bcb334ba77a8a20cd0f8ac4292df
sloobman <user@email.com>
2026-03-28 12:07:52 +0300

    Add ValidationModule implementing IModule with service registration and logging

---
+++ Core/ValidationModule.cs
@@ -0,0 +1,13 @@
+public class ValidationModule : IModule
+{
+    public string Name => "Validation";
+    public IEnumerable<string> Dependencies => new[] { "Logging" };
+
+    public void RegisterServices(IServiceCollection services) { }
+
+    public void Initialize(IServiceProvider provider)
+    {
+        var logger = provider.GetRequiredService<ILoggerService>();
+        logger.Log("Validation executed");
+    }
+}
