d258c81e555a4493e50ebf0d542bdd1f0e22e144
sloobman <user@email.com>
2026-03-28 12:07:37 +0300

    Add LoggingModule with ILoggerService and LoggerService implementation

---
+++ Core/LoggingModule.cs
@@ -0,0 +1,29 @@
+public interface ILoggerService
+{
+    void Log(string message);
+}
+
+public class LoggerService : ILoggerService
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG] {message}");
+    }
+}
+
+public class LoggingModule : IModule
+{
+    public string Name => "Logging";
+    public IEnumerable<string> Dependencies => new string[] { };
+
+    public void RegisterServices(IServiceCollection services)
+    {
+        services.AddSingleton<ILoggerService, LoggerService>();
+    }
+
+    public void Initialize(IServiceProvider provider)
+    {
+        var logger = provider.GetRequiredService<ILoggerService>();
+        logger.Log("Logging initialized");
+    }
+}
